3dfbad151bd29c18756fd420daf0b4ca28370510
Bilal <workubilal@gmail.com>
2024-12-25 09:19:31 +0300

    first few, i'm getting my hands dirty.

--- 002-progress steps/script.js
+++ 002-progress steps/script.js
@@ -1,30 +1,38 @@
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
+// select all classes w name circle.
 const circles = document.querySelectorAll(".circle");
 
 let currentActive = 1;
-
+ 
+// when the btn is clicked, + currentActive by 1 and run the update function.
 next.addEventListener("click", () => {
-  currentActive++;
+  currentActive++; 
   if (currentActive > circles.length) currentActive = circles.length;
   update();
 });
 
+// when the button is clicked, - by 1 and run the update function.
 prev.addEventListener("click", () => {
   currentActive--;
   if (currentActive < 1) currentActive = 1;
   update();
 });
 
+// iterate over the circle list, make active for those less than current.
 const update = () => {
   circles.forEach((circle, index) => {
     if (index < currentActive) circle.classList.add("active");
     else circle.classList.remove("active");
   });
+
+  // select all active classes, and count the number of actives.
   const actives = document.querySelectorAll(".active");
+  // the total are all 4, and set the width in 1/3, 2/3, 3/3 *actives*
   progress.style.width =
     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+    // toggle btns to disabled based on condition.
   if (currentActive === 1) prev.disabled = true;
   else if (currentActive === circles.length) next.disabled = true;
   else {
--- 014-animated navigation/script.js
+++ 014-animated navigation/script.js
@@ -1,4 +1,5 @@
 const toggle = document.getElementById("toggle");
 const nav = document.getElementById("nav");
 
+// from click event toggle the active class.
 toggle.addEventListener("click", () => nav.classList.toggle("active"));
